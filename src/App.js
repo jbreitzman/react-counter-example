cc0a3b62a1a8a4d2d2c9f5e76b361937f0a3292d
jbreitzman <jbreitzman@gmail.com>
2020-08-03 08:59:28 -0400

    Organized unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import CounterButton from "./components/counterButton/CounterButton";
 import Counter from "./components/counter/Counter";
 import "./App.css";
 
